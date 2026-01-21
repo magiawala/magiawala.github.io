7d580642c44dc254dcb0f725fdf854c4efe57f1b
Devanshu Magiawala <devanshumagiawala@Devanshus-Laptop.local>
2026-01-20 21:11:25 -0800

    Add Think Aloud analysis image to HP Case Study

--- src/pages/HPCaseStudy.tsx
+++ src/pages/HPCaseStudy.tsx
@@ -18,8 +18,8 @@ import recommendationsVideo from "@/assets/Recommendations.webm";
 import fullPrototypeVideo from "@/assets/Full Prototype.webm";
 
 
-import verizonDesignSystem from "@/assets/verizon-design-system.png";
 import HPBanner from "@/assets/HP banner.png";
+import hpThinkAloud from "@/assets/hp-think-aloud.png";
 import LazyVideo from "@/components/LazyVideo";
 
 const HPCaseStudy = () => {
